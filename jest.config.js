12ef84c97eb4031a754f681dd186e0c97e64eb6f
kagawagao <kingsongao1221@gmail.com>
2020-10-12 10:45:20 +0800

    chore: update build config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: '@opd/jest-preset-pangu',
+  transformIgnorePatterns: ['/node_modules/', '/src/.umi/'],
+  collectCoverageFrom: [
+    './src/**/*.{ts,tsx}',
+    '!./**/*.d.ts',
+    '!src/.umi/**/*.ts',
+  ],
 }
